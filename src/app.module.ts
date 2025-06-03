c606f51a15f347796b1415b06a11446063cb2ab2
Ricardo180206 <rjsigua@sudamericano.edu.ec>
2025-06-03 18:42:30 -0500

    no se que hacer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsModule } from './notifications/notifications.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ric@rdo1802',
-      database: 'notification_db',
+      url: process.env.DATABASE_URL, // Utiliza la variable de entorno DATABASE_URL
       autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // Requerido para conexiones SSL en Render
+      },
     }),
     NotificationsModule,
- 
   ],
-
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
