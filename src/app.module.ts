5855e696a9614dad181156f77f48ae139a10f8b5
Ricardo180206 <rjsigua@sudamericano.edu.ec>
2025-06-09 20:58:24 -0500

    nuevo codigo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsModule } from './notifications/notifications.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ric@rdo1802',
-      database: 'notification_db',
-      autoLoadEntities: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     NotificationsModule,
- 
   ],
-
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
