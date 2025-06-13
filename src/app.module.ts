3631a926ee98a540671ed3a0426751119cd389a3
Ricardo180206 <rjsigua@sudamericano.edu.ec>
2025-06-13 18:55:06 -0500

    REBDER 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 👈 carga las variables de entorno desde .env o Render
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ric@rdo1802',
-      database: 'notification_db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'production', // Solo sincroniza si NO estás en producción
     }),
     NotificationsModule,
- 
   ],
-
 })
 export class AppModule {}
-
